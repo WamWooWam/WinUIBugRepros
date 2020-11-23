bf74d5abba27751bf710757e3d74ae6ff7392078
Thomas May <wamwoowam@gmail.com>
2020-11-23 18:58:04 +0000

    AccessViolation repro app

---
+++ ReproApp1/ReproApp1/ItemControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ReproApp
+{
+    public sealed class ItemControl : Control
+    {
+        public ItemControl()
+        {
+            this.DefaultStyleKey = typeof(ItemControl);
+        }
+
+        public Item Item
+        {
+            get { return (Item)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemProperty =
+            DependencyProperty.Register("Item", typeof(Item), typeof(ItemControl), new PropertyMetadata(null));
+    }
+}
---
+++ ReproApp1/ReproApp1/ViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReproApp
+{
+    public class Item
+    {
+        public Item()
+        {
+            String1 = "String1";
+            String2 = "String2";
+            String3 = "String3";
+            String4 = "String4";
+            String5 = "String5";
+        }
+
+        public string String1 { get; set; }
+        public string String2 { get; set; }
+        public string String3 { get; set; }
+        public string String4 { get; set; }
+        public string String5 { get; set; }
+
+        public SubItem SubItem { get; set; } = new SubItem();
+    }
+
+    public class SubItem
+    {
+        public SubItem()
+        {
+            String1 = "String1";
+            String2 = "String2";
+            String3 = "String3";
+            String4 = "String4";
+            String5 = "String5";
+        }
+
+        public string String1 { get; set; }
+        public string String2 { get; set; }
+        public string String3 { get; set; }
+        public string String4 { get; set; }
+        public string String5 { get; set; }
+    }
+
+    public class ViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public ViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            for (int i = 0; i < 1000; i++)
+            {
+                Items.Add(new Item());
+            }
+        }
+
+    }
+}
